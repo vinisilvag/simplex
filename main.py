f40f920664d112c02df2221b2c9b7830ac8e4c51
Vinicius Gomes <vinicius.svgomes@gmail.com>
2023-11-22 09:30:38 -0300

    format and infeasible

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def read_input():
     b = A[:, m]
     A = A[:, :m]
 
+    A, b, c = A.astype(float), b.astype(float), c.astype(float)
+
     return n, m, A, b, c
 
 
@@ -30,30 +32,32 @@ def standard_equality_form(n, m, A, b, c):
 
     A = np.concatenate((A, np.eye(n)), axis=1)
 
+    invert = []
+
     for i in range(n):
         if b[i] < 0:
-            b[i] *= -1
-            A[i, :] *= -1
+            invert.append(i + 1)
+            b[i] = -b[i]
+            A[i, :] = -A[i, :]
 
     c = np.concatenate((c, np.zeros(n)))
 
     b = np.reshape(b, (n, 1))
     c = np.reshape(c, (1, m))
 
-    return n, m, A, b, c
+    return n, m, A, b, c, invert
 
 
-def canonical(T, bases):
+def canonical(T, i, j):
     n = T.shape[0] - 1
 
-    for i in range(n):
-        T[bases[i][0], :] /= T[bases[i][0]][bases[i][1]]
+    T[i, :] /= T[i, j]
 
-        for j in range(T.shape[0]):
-            if j == bases[i][0]:
-                continue
+    for k in range(T.shape[0]):
+        if i == k:
+            continue
 
-            T[j, :] -= T[bases[i][0], :] * T[j][bases[i][1]]
+        T[k, :] -= (T[i, :] * T[k, j])
 
     return T
 
@@ -71,11 +75,14 @@ def simplex_iteration(T, bases):
             break
 
         pivot_row = -1
-        min_val = np.finfo(np.float64).max
+        min_val = float('inf')
 
         for i in range(1, T.shape[0]):
-            if T[i][improving_col] > 0 and T[i][-1:] / T[i][improving_col] < min_val:
-                min_val = T[i][-1:] / T[i][improving_col]
+            if T[i][improving_col] <= 0:
+                continue
+
+            if T[i][-1] / T[i][improving_col] < min_val:
+                min_val = T[i][-1] / T[i][improving_col]
                 pivot_row = i
 
         if pivot_row == -1:
@@ -83,12 +90,12 @@ def simplex_iteration(T, bases):
 
         bases[pivot_row - 1] = (pivot_row, improving_col)
 
-        canonical(T, bases)
+        canonical(T, pivot_row, improving_col)
 
     return False, T, bases
 
 
-def simplex(n, m, A, b, c):
+def simplex(n, m, A, b, c, invert):
     # VERO
     aux_T = np.vstack((np.zeros(n), np.eye(n)))
 
@@ -99,18 +106,23 @@ def simplex(n, m, A, b, c):
         np.vstack((np.zeros(1), b))  # b
     ))
 
+    for i in invert:
+        aux_T[i, :n] = -aux_T[i, :n]
+
     rows = list(range(1, n+1))
     columns = list(range(n + m, n + m + n))
 
     bases = np.array(list((rows[i], columns[i])
                      for i in range(n)), dtype="i,i")
 
-    aux_T = canonical(aux_T, bases)
+    for i, j in bases:
+        canonical(aux_T, i, j)
 
     _, aux_T, bases = simplex_iteration(aux_T, bases)
+    optimal = aux_T[0, -1]
 
-    if aux_T[0, -1:] < 0:
-        return "inviavel", (), (-aux_T[0, 0:n]), ()
+    if optimal < 0:
+        return "inviavel", (), (aux_T[0, :n]), ()
     else:
         T = aux_T.copy()
         T = np.delete(T, list(range(n + m, n + m + n)), 1)
@@ -126,37 +138,39 @@ def simplex(n, m, A, b, c):
             for i in range(n):
                 index = bases[i][1] - n
                 if index >= 0 and index < m:
-                    x[index] = T[bases[i][0], -1:]
+                    x[index] = T[bases[i][0], -1]
 
-            return "otima", (T[0, -1], x, T[0, 0:n]), (), ()
+            return "otima", (T[0, -1], x, T[0, :n]), (), ()
 
 
 def main():
     n, m, A, b, c = read_input()
 
-    n, m, A, b, c = standard_equality_form(n, m, A, b, c)
+    n, m, A, b, c, invert = standard_equality_form(n, m, A, b, c)
 
-    classification, optimum, infeasible, unbounded = simplex(n, m, A, b, c)
+    classification, optimum, infeasible, unbounded = simplex(
+        n, m, A, b, c, invert
+    )
 
     print(classification)
 
     if classification == "otima":
         optimal, x, certificate = optimum
 
-        print(optimal)
+        print("{:.7f}".format(optimal))
 
         for i in x:
-            print(i, end=' ')
+            print("{:.7f}".format(i), end=' ')
 
         print()
 
         for i in certificate:
-            print(i, end=' ')
+            print("{:.7f}".format(i), end=' ')
     elif classification == "inviavel":
         certificate = infeasible
 
         for i in certificate:
-            print(i, end=' ')
+            print("{:.7f}".format(i), end=' ')
     else:
         pass
 
